7931097d6d1469aa63fc0037a1fe57564a9daaaa
Anthony Chen <a.z.chen@student.tudelft.nl>
2024-04-26 12:30:33 +0200

    add sensor data functions

--- KITT.py
+++ KITT.py
@@ -1,5 +1,9 @@
 import serial
 import keyboard
+import time
+import re
+import numpy as np
+import csv
 
 class KITT:
     def __init__(
@@ -12,7 +16,8 @@ class KITT:
         self.serial = serial.Serial(port, baudrate, rtscts=True)
 
         # initialize audio beacon
-        self.start_beacon()             # turn on beacon
+        #self.start_beacon()             # turn on beacon
+        self.send_command(f'A0\n')      # turn off beacon
 
         carrier_frequency = carrier_frequency.to_bytes(2, byteorder='big')
         bit_frequency = bit_frequency.to_bytes(2, byteorder='big')
@@ -23,6 +28,11 @@ class KITT:
         self.serial.write(b'B' + bit_frequency + b'\n')
         self.serial.write(b'R' + repitition_count + b'\n')
         self.serial.write(b'C' + code + b'\n')
+
+        #self.delays = []
+        self.sensor_data = [['time', 'dist_l', 'dist_r']]
+        self.start_time = time.time()
+
 
         # state variables such as speed, angle are defined here
 
@@ -46,34 +56,65 @@ class KITT:
     def stop_beacon(self):
         self.send_command(f'A0\n')      # turn off beacon
 
+    def read_sensors(self):
+        start = time.time()
+        t_plus = start - self.start_time
+
+        self.send_command(f'Sd\n')
+        status = self.serial.read_until(b'\x04')
+        print(status)
+
+        temp = status.decode()
+        temp = re.findall(r'\d+', temp)
+        res = list(map(int, temp))
+        dist_l = res[0]
+        dist_r = res[1]
+        print(dist_l)
+        print(dist_r)
+
+        end = time.time()
+        duration = end - start
+        print(f"{duration} seconds")
+        #self.delays.append(duration)
+
+        self.sensor_data.append([t_plus, dist_l, dist_r])
+
+    def save_sensor_data(self):
+        with open("sensors.csv", "w", newline="") as file:
+            mywriter = csv.writer(file, delimiter=",")
+            mywriter.writerows(self.sensor_data)
+
     def __del__(self):
         self.stop()                     # stop car
         self.send_command(f'A0\n')      # turn off beacon
         self.serial.close()             # close serial connection
+
+        print(self.sensor_data)
+        #print(delays)
 
 def wasd(kitt):
     while True:
         event = keyboard.read_event()
 
         if event.event_type == keyboard.KEY_DOWN and event.name == 'w':
-            print("going forward")
+            #print("going forward")
             kitt.set_speed(165)
         if event.event_type == keyboard.KEY_DOWN and event.name == 's':
-            print("going backwards")
+            #print("going backwards")
             kitt.set_speed(135)
         if event.event_type == keyboard.KEY_DOWN and event.name == 'a':
-            print("going left")
+            #print("going left")
             kitt.set_angle(200)
         if event.event_type == keyboard.KEY_DOWN and event.name == 'd':
-            print("going right")
+            #print("going right")
             kitt.set_angle(100)
 
         # stop car or reset wheels when keys are released
         if event.event_type == keyboard.KEY_UP and (event.name == 'w' or event.name == 's'):
-            print("stopping motor")
+            #print("stopping motor")
             kitt.set_speed(150)
         if event.event_type == keyboard.KEY_UP and (event.name == 'a' or event.name == 'd'):
-            print("resetting wheel angle")
+            #print("resetting wheel angle")
             kitt.set_angle(150)
 
         if event.event_type == keyboard.KEY_DOWN and event.name == 'e':
@@ -83,6 +124,12 @@ def wasd(kitt):
             print("stopping beacon")
             kitt.stop_beacon()
 
+        if event.event_type == keyboard.KEY_DOWN and event.name == 'r':
+            kitt.read_sensors()
+        if event.event_type == keyboard.KEY_DOWN and event.name == 't':
+            print("saving sensor data..")
+            kitt.save_sensor_data()
+
         # exit loop when escape is pressed
         if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
             print("exiting...")
@@ -91,6 +138,7 @@ def wasd(kitt):
 
 if __name__ == "__main__":
     # test code follows here
-    comport = input("Please enter your comport: ")
+    #comport = input("Please enter your comport: ")
+    comport = "/dev/rfcomm0"
     kitt = KITT(comport)
     wasd(kitt)
