c1c60d2717155a89dacc1d0012857a5c999c1c12
Anthony Chen <a.z.chen@student.tudelft.nl>
2024-04-26 08:51:37 +0200

    add wasd controls to KITT

--- KITT.py
+++ KITT.py
@@ -19,13 +19,39 @@ class KITT:
         self.set_speed(150)
         self.set_angle(150)
 
-    def __del__(self)
+    def __del__(self):
         self.serial.close()
 
 def wasd(kitt):
-    # add your code
+    while True:
+        event = keyboard.read_event()
+        if event.event_type == keyboard.KEY_DOWN and event.name == 'w':
+            print("going forward")
+            kitt.set_speed(165)
+        if event.event_type == keyboard.KEY_DOWN and event.name == 's':
+            print("going backwards")
+            kitt.set_speed(135)
+        if event.event_type == keyboard.KEY_DOWN and event.name == 'a':
+            print("going left")
+            kitt.set_angle(200)
+        if event.event_type == keyboard.KEY_DOWN and event.name == 'd':
+            print("going right")
+            kitt.set_angle(100)
+
+        if event.event_type == keyboard.KEY_UP and (event.name == 'w' or event.name == 's'):
+            print("stopping motor")
+            kitt.set_speed(150)
+        if event.event_type == keyboard.KEY_UP and (event.name == 'a' or event.name == 'd'):
+            print("resetting wheel angle")
+            kitt.set_angle(150)
+
+        if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
+            print("exiting...")
+            kitt.stop()
+            break
 
 if __name__ == "__main__":
     # test code follows here
-    kitt = KITT("/dev/rfcomm0")
+    comport = input("Please enter your comport: ")
+    kitt = KITT(comport)
     wasd(kitt)
