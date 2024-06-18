0f6a150f155d0113add3dc7fa58fb48d9827fb9c
Anthony Chen <a.z.chen@student.tudelft.nl>
2024-06-18 17:16:51 +0200

    enter parameters in console

--- control.py
+++ control.py
@@ -6,6 +6,7 @@ import KITTloc
 from wavaudioread import wavaudioread
 import KITTmodel
 import KITT
+import datetime
 
 def theta_to_angle_1(theta_in):
     max_theta = 30
@@ -33,19 +34,16 @@ c = 0.1
 L = 0.335
 x0 = 0.2
 y0 = 0.25
-x0 = 4.6
-y0 = 4.6
 alpha0 = 90
 v0 = 0
 
 kitt_model = KITTmodel.KITTmodel(m, b, c, L, x0, y0, alpha0, v0)
 
-#y_ref = np.array(wavaudioread("ref tests/5000-1000-87.wav", 44100))
-#loc = KITTloc.Localization(False, y_ref, 1, 20000)
-
 COM = "/dev/rfcomm0"
 
-enable_KITT = False
+enable_KITT = input("Enter y if you want to enable KITT: ") == 'y'
+loc_on = input("Enter y if you want to enable localization: ") == 'y'
+
 if enable_KITT:
 # list audio devices
     pyaudio_handle = pyaudio.PyAudio()
@@ -74,29 +72,20 @@ loc_x_list = [x0]
 loc_y_list = [y0]
 
 
-#xA = 4.0
-#yA = 2.3
-xA = 2.3
-yA = 2.3
+goal_list = []
+goals = input("Enter your coordinates like this: x,y;x,y;xy;... : ")
+goals = goals.split(";")
+for goal in goals:
+    goal = goal.split(",")
+    goal_list.append(np.array((float(goal[0]),float(goal[1]))))
 
-xB = 2
-yB = 4
+goal_index = 0
 
-#x_loc = [0.294, 0.36, 0.371]
-#y_loc = [0.486, 0.816, 1.139]
-#loc_i = 0
-
-is_chB = True
-b_finished = False
-
-x_goal = xA
-y_goal = yA
-goal = np.array((x_goal, y_goal))
+goal = goal_list[goal_index]
 input("Press enter to start...")
 
 loc_interval = 2
 logging_interval = 0.1
-loc_on = False
 
 start_time = time.time()
 current_time = start_time
@@ -104,11 +93,9 @@ old_time = current_time
 last_loc_time = start_time
 finished = False
 
-max_time = 5
-if is_chB:
-    max_time = 2*max_time + 10
+max_time = 300
 
-stopped = True
+last_goal = goal_index == len(goal_list)-1
 while not finished:
     if enable_KITT:
         if kitt.logging and current_time - old_time > logging_interval:
@@ -123,21 +110,15 @@ while not finished:
 
         new_d = np.array((model_x-loc_x_list[-1],model_y-loc_y_list[-1]))
         dist_to_loc = np.linalg.norm(new_d)
-        #print("dt:", current_time-last_loc_time)
-        if current_time - last_loc_time > loc_interval:
         #if dist_to_loc > 0.5:
+        if current_time - last_loc_time > loc_interval:
             if enable_KITT:
                 kitt.ebrake()
             kitt_model.ebrake()
-            stopped = True
             old_time = time.time()
             print("Localizing...")
             duration = 0.6 # in seconds
 
-            #est_x = x_loc[loc_i]
-            #est_y = y_loc[loc_i]
-            #if (loc_i < 2):
-            #    loc_i += 1
             est_x, est_y = float('nan'), float('nan')
             invalid = True
             invalid_count = 0
@@ -186,16 +167,16 @@ while not finished:
         if enable_KITT:
             kitt.ebrake()
         kitt_model.ebrake()
-        stopped = True
-        if (is_chB and (not b_finished)):
+        if (not last_goal):
             b_finished = True
             print("Waiting 10 seconds...")
             if enable_KITT:
                 time.sleep(10)
             old_time = time.time()
-            x_goal = xB
-            y_goal = yB
-            goal = np.array((x_goal, y_goal))
+            goal_index += 1
+            goal = goal_list[goal_index]
+            if goal_index == len(goal_list)-1:
+                last_goal = True
 
             d_goal = goal - kitt_model.get_x()
             distance = np.linalg.norm(d_goal)
@@ -239,10 +220,6 @@ while not finished:
     kitt_model.set_angle(angle)
     kitt_model.set_speed(speed)
 
-    #if (stopped) :
-    #    time.sleep(0.5)
-    #    stopped = False
-
     x_list.append(kitt_model.x[0])
     y_list.append(kitt_model.x[1])
     alpha_list.append(kitt_model.alpha)
@@ -261,7 +238,6 @@ while not finished:
         if enable_KITT:
             kitt.ebrake()
         kitt_model.ebrake()
-        stopped = True
         finished = True
 
 if enable_KITT:
@@ -275,9 +251,8 @@ fig,a = plt.subplots(2,2, figsize=(10,10))
 a[0][0].scatter(x_list, y_list, color='red', s = 1)
 a[0][0].scatter(loc_x_list, loc_y_list, color='yellow')
 a[0][0].scatter(x0, y0, color='blue')
-a[0][0].scatter(xA, yA, color='green')
-if is_chB:
-    a[0][0].scatter(xB, yB, color='pink')
+goal_list = np.transpose(goal_list)
+a[0][0].scatter(goal_list[0],goal_list[1], color='green')
 a[0][0].set_xlabel("X position [m]")
 a[0][0].set_ylabel("Y Position [m]")
 a[0][0].set_title("Position on the field")
@@ -290,7 +265,7 @@ a[1][0].set_xlabel("Time [s]")
 a[1][0].set_ylabel("Orientation [degrees]")
 a[1][0].set_title("Orientation of the car")
 
-#a[0][1].plot(t_list, dist_list)
+a[0][1].plot(t_list, dist_list)
 #a[0][1].plot(t_list, theta_list)
 #a[0][1].plot(t_list, phi_goal_list)
 a[0][1].set_xlabel("Time [s]")
@@ -298,11 +273,12 @@ a[0][1].set_xlabel("Time [s]")
 a[0][1].set_ylabel("Distance [m]")
 a[0][1].set_title("Distance to the goal")
 
-#a[1][1].plot(t_list, v_list)
-a[1][1].plot(t_list, theta_list)
+a[1][1].plot(t_list, v_list)
+#a[1][1].plot(t_list, theta_list)
 a[1][1].set_xlabel("Time [s]")
 a[1][1].set_ylabel("Velocity [m/s]")
 a[1][1].set_title("Velocity over time")
 
-#plt.savefig("plots/fullmodel.pdf")
+timestamp = datetime.datetime.now().replace(microsecond=0).isoformat()
+plt.savefig("plots/challenge_%s.pdf" % timestamp)
 plt.show()
